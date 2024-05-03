fa392b8283646808f25ffce1b447179bc40201ad
Martin Krüger <74255336+martinkr2k@users.noreply.github.com>
2024-05-03 11:34:00 +0200

    Update led_switch.vhd

--- led_switch.vhd
+++ led_switch.vhd
@@ -1,16 +1,1 @@
-library ieee;
-use ieee.std_logic_1164.all;
 
-entity led_switch is 
-	port (
-		sw : in std_logic_vector(9 downto 0);
-		ledr : out std_logic_vector(9 downto 0)
-	);
-end led_switch;
-
-architecture behavior of led_switch is 
-	begin 
-	
-		ledr <= sw;
-	
-end behavior;
