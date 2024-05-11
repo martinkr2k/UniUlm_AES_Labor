9a0005653e28a18ed3868eabe6040bb702a119b4
Martin Krüger <martinkr.rst@gmail.com>
2024-05-11 16:01:45 +0200

    L3 A1

--- L3/A1/vhdl/rs_latch.vhdl
+++ L3/A1/vhdl/rs_latch.vhdl
@@ -2,28 +2,32 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity rs_latch is 
-	port (
-		p_r, p_s : in std_logic;
-		p_clk : in std_logic;
-		p_q : out std_logic
-	);
+  port (
+    p_clk : in std_logic;
+    p_r : in std_logic;
+    p_s : in std_logic;
+    p_q : out std_logic
+  );
 end rs_latch;
 
 
 architecture STRUCTURAL of rs_latch is 
-	signal s_r_g, s_r_g : std_logic;
-	signal s_qa, s_qb : std_logic;
-	attribute KEEP : BOOLEAN;
-	attribute KEEP OF s_r_g, s_s_g, s_qa, s_qb : SIGNAL IS TRUE;
-	
-begin
+  signal s_rg, s_sg : std_logic;
+  signal s_qa, s_qb : std_logic;  
+  ATTRIBUTE KEEP : BOOLEAN;
+  ATTRIBUTE KEEP OF s_rg, s_sg, s_qa, s_qb : SIGNAL IS TRUE;
 
-	s_r_g <= p_r AND p_clk;
-	s_s_g <= p_s AND p_clk;
+  begin 
 
-	s_qa <= not(s_r_g OR s_qb);
-	s_qb <= not(s_s_g OR s_qa);
+    s_rg <= p_r AND p_clk;
+    s_sg <= p_s AND p_clk;
+    
+    --s_rg <= p_r;
+    --s_sg <= p_s;
 
-	p_q <= s_qa;
+    s_qa <= not(s_rg OR s_qb);
+    s_qb <= not(s_sg OR s_qa);
+    
+    p_q <= s_qa;
 
 end STRUCTURAL;
---
+++ L3/A1/vhdl/rs_latch_tb.vhdl
@@ -0,0 +1,101 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity rs_latch_tb is 
+end rs_latch_tb;
+
+
+architecture TB of rs_latch_tb is 
+  signal s_clk, s_r, s_s : std_logic;
+  signal s_q : std_logic; 
+
+  begin 
+
+    RSL : entity work.rs_latch(STRUCTURAL) port map (s_clk, s_r, s_s, s_q);
+    
+    process 
+    begin 
+
+      -- TC 1
+      s_clk <= '1';
+      s_r <= '0';
+      s_s <= '1';
+      wait for 2 ns;
+      assert (s_q = '1') report "TC 1: initial set!";
+      
+
+      -- TC 2
+      s_clk <= '0';
+      s_r <= '0';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '1') report "TC 2: wait!";
+      
+      s_clk <= '1';
+      s_r <= '0';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '1') report "TC 2: wait!";
+
+
+      -- TC 3
+      s_clk <= '0';
+      s_r <= '1';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '1') report "TC 3: reset!";
+
+      s_clk <= '1';
+      s_r <= '1';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '0') report "TC 3: reset!";
+
+
+      -- TC 4
+      s_clk <= '0';
+      s_r <= '0';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '0') report "TC 4: wait!";
+
+      s_clk <= '1';
+      s_r <= '0';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '0') report "TC 4: wait!";
+
+
+      -- TC 5
+      s_clk <= '0';
+      s_r <= '0';
+      s_s <= '1';
+      wait for 2 ns;
+      assert (s_q = '0') report "TC 5: set!";
+
+      s_clk <= '1';
+      s_r <= '0';
+      s_s <= '1';
+      wait for 2 ns;
+      assert (s_q = '1') report "TC 5: set!";
+
+
+      -- TC 6
+      s_clk <= '0';
+      s_r <= '0';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '1') report "TC 6: wait!";
+      
+      s_clk <= '1';
+      s_r <= '0';
+      s_s <= '0';
+      wait for 2 ns;
+      assert (s_q = '1') report "TC 6: wait!";
+      
+      report "end of test";
+      wait;
+
+    end process;
+
+end TB;
