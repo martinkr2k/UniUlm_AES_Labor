b3b508098917023154590999e6036bf0bf0335da
Martin Krüger <74255336+martinkr2k@users.noreply.github.com>
2024-07-12 13:31:07 +0200

    L9 Abgabe

---
+++ L9/Abgabe/hello_world.c
@@ -0,0 +1,199 @@
+/*
+ * "Hello World" example.
+ *
+ * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
+ * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
+ * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
+ * device in your system's hardware.
+ * The memory footprint of this hosted application is ~69 kbytes by default
+ * using the standard reference design.
+ *
+ * For a reduced footprint version of this template, and an explanation of how
+ * to reduce the memory footprint for a given application, see the
+ * "small_hello_world" template.
+ *
+ */
+
+#include <stdio.h>
+#include "system.h"
+#include "alt_types.h"
+#include "io.h"
+#include <sys/time.h>
+#include <sys/alt_irq.h>
+#include "altera_avalon_performance_counter.h"
+
+
+/*
+alt_u32 crc_checker(alt_u32 message, alt_u8 polynom)
+{
+
+	alt_u32 result = message;
+
+	for (int i = 31; i >= 7; i--)
+	{
+		if (result & (1 << i)) {
+			result ^= (alt_u32)polynom << (i - 7);
+		}
+	}
+
+	return result;
+}
+
+
+
+int main()
+{
+	printf("Hello from Nios II CRC CHECKER!\n");
+	PERF_RESET(PERFORMANCE_COUNTER_0_BASE);
+	PERF_START_MEASURING(PERFORMANCE_COUNTER_0_BASE);
+
+	// VARIABLES
+	alt_u32 messageInput = 0;
+	alt_u8 polynomInput = 0;
+	struct timeval tv1, tv2;
+
+	// READ INPUT
+	printf("\nMESSAGE: ");
+
+	scanf("%x", &messageInput);
+
+	printf("\nPOLYNOM: ");
+
+	scanf("%x", &polynomInput);
+
+	printf("\nMessage: ");
+	for (int i = 31; i >= 0; --i) {
+		if (messageInput & (1 << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+	}
+	printf("\n");
+
+	printf("Polynom: ");
+	for (int i = 7; i >= 0; --i) {
+		if (polynomInput & (1 << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+	}
+	printf("\n");
+	gettimeofday(&tv1, NULL);
+	PERF_BEGIN(PERFORMANCE_COUNTER_0_BASE, 1);
+	alt_u32 result = crc_checker(messageInput, polynomInput);
+	PERF_END(PERFORMANCE_COUNTER_0_BASE, 1);
+	PERF_STOP_MEASURING(PERFORMANCE_COUNTER_0_BASE);
+	gettimeofday(&tv2, NULL);
+
+	//printf("%lu\n");
+	printf("Result:  ");
+	for (int i = 31; i >= 0; --i) {
+		if (result & (1 << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+	}
+	printf("\n");
+	printf("Time: %f \n", (double) (tv2.tv_usec - tv1.tv_usec) / 100000 + (double) (tv2.tv_sec - tv1.tv_sec));
+
+	perf_print_formatted_report(PERFORMANCE_COUNTER_0_BASE, ALT_CPU_FREQ, 1, "crc_checker");
+
+  return 0;
+}
+*/
+
+
+
+int main()
+{
+
+	printf("Hello from Nios II new new!\n");
+
+	struct timeval tv1, tv2;
+	alt_u32 message = 0;
+	alt_u32 polynom = 0;
+	PERF_RESET(PERFORMANCE_COUNTER_0_BASE);
+	PERF_START_MEASURING(PERFORMANCE_COUNTER_0_BASE);
+
+
+	// EINGABE
+	printf("Input Message:\n");
+	scanf("%x", &message);
+	printf("\nInput Polynom:\n");
+	scanf("%x", &polynom);
+
+	printf("\nInputs in Bit: \n");
+	printf("\nMessage: ");
+	for (int i = 31; i >= 0; --i) {
+		if (message & (1 << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+	}
+
+	printf("\nPolynom: ");
+	for (int i = 31; i >= 0; --i) {
+		if (polynom & (1 << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+	}
+	printf("\n");
+
+	printf("\n-----\n");
+	// ENDE EINGABE
+
+
+	// MESSAGE & POLYNOM WRITEN
+	IOWR(CRC_0_BASE, 0, message);
+	IOWR(CRC_0_BASE, 1, polynom);
+
+	PERF_BEGIN(PERFORMANCE_COUNTER_0_BASE, 1);
+
+	// HARDWARE RECHNET
+	alt_u32 result;
+	alt_u32 result1;
+
+	// WARTEN AUF PROZESSIERUNG
+	do {
+	   result1 = IORD(CRC_0_BASE, 1);
+	} while (result1 & (1<<0));
+
+	PERF_END(PERFORMANCE_COUNTER_0_BASE, 1);
+	PERF_STOP_MEASURING(PERFORMANCE_COUNTER_0_BASE);
+
+	// AUSGABE DER MESSAGE
+	result = IORD(CRC_0_BASE, 0);
+
+
+	// AUSGABE
+	printf("\nResult Message Register:  ");
+	for (int i = 31; i >= 0; --i) {
+		if (result & (1 << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+	}
+
+	printf("\nResult Polynom Register:  ");
+	for (int i = 31; i >= 0; --i) {
+		if (result1 & (1 << i)) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+	}
+
+	printf("\n\n\n");
+	perf_print_formatted_report(PERFORMANCE_COUNTER_0_BASE, ALT_CPU_FREQ, 1, "crc_checker");
+	// ENDE AUSGABE
+
+	return 0;
+}
+
---
+++ L9/Abgabe/task_1.vhd
@@ -0,0 +1,30 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+ENTITY task_1 IS
+	PORT (
+		CLOCK_50 : IN STD_LOGIC;
+		KEY : IN STD_LOGIC_VECTOR (0 DOWNTO 0);
+		SW : IN STD_LOGIC_VECTOR (9 DOWNTO 0);
+		LEDR : OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
+	);
+END task_1;
+
+ARCHITECTURE task_rtl OF task_1 IS
+	COMPONENT nios_system
+		PORT (
+			SIGNAL clk_clk: IN STD_LOGIC;
+			SIGNAL reset_reset_n : IN STD_LOGIC
+		);
+	END COMPONENT;
+	
+	BEGIN
+		NiosII : nios_system
+		PORT MAP(
+			clk_clk => CLOCK_50,
+			reset_reset_n => KEY(0)
+			--switches_export => SW(9 DOWNTO 0),
+			--leds_export => LEDR(9 DOWNTO 0)
+		);
+END task_rtl;
